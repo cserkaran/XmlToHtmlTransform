6a13b230a6bedfa186f5ad7e1772a8ca58367538
Karan Sharma <cserkaran@gmail.com>
2017-02-05 21:32:53 +0530

    Added helper method to Transform XML To HTML.

---
+++ Source/Utilities/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Helper class containing utility method's to be used 
+    /// accross the application.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Transforms the XML to HTML.
+        /// </summary>
+        /// <param name="inputXml">The xml text.</param>
+        /// <param name="xsltString">The XSLT text.</param>
+        /// <returns>the transformed html text.</returns>
+        public static string TransformXMLToHTML(string inputXml, string xslt)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            // set DTD processing to parse.
+            using (XmlReader reader =
+                XmlReader.Create(new StringReader(xslt),
+                new XmlReaderSettings() { DtdProcessing = DtdProcessing.Parse }))
+            {
+                transform.Load(reader);
+            }
+            StringWriter results = new StringWriter();
+            using (XmlReader reader = XmlReader.Create(new StringReader(inputXml)))
+            {
+                transform.Transform(reader, null, results);
+            }
+            return results.ToString();
+        }
+    }
+}
