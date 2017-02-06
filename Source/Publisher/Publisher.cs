f431cd638a0d09e7984135323090e13034cb70b9
Karan Sharma <cserkaran@gmail.com>
2017-02-06 10:50:07 +0530

    Adding dependency injection via MEF.

--- Source/Publisher/Publisher.cs
+++ Source/Publisher/Publisher.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using Contracts;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
 
 namespace Publisher
 {
+    [Export(typeof(IPublisher))]
     public class Publisher : IPublisher
     {
         public event EventHandler<XmlContentEventArgs> Published;
+
+        private void OnPublish()
+        {
+            Published?.Invoke(this, new XmlContentEventArgs(new List<XmlContent>()));
+        }
+
     }
 }
--- Source/Subscriber/Subscriber.cs
+++ Source/Subscriber/Subscriber.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Contracts;
+using System.ComponentModel.Composition;
 
 namespace Subscriber
 {
+    [Export(typeof(ISubscriber))]
     public class Subscriber : ISubscriber
     {
         public void Notify(IList<XmlContent> xmlContents)
